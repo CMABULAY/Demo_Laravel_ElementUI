cb2897487c05c03499df8395c807d1d178880d29
CYRUSM23 <cyrusmabulay@gmail.com>
2021-07-09 00:05:18 +0800

    Update home.vue

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -5,7 +5,7 @@
     1. npm install <br>
     2. create database package named 'DBDemo'<br>
     3. migrate tables<br>
-    4. you can test all :) <br>
+    4. php artisan serve, npm run watch.  you can test all :) <br>
 
   </el-card>
 </template>
